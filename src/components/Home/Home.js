e900e2ffddebb3fdda3a7ac4ee951ef720565ec9
Rustam <rustam905@gmail.com>
2021-05-10 10:25:41 -0700

    Refractored input

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,14 +3,14 @@ import AuthContext from "../../store/auth-context";
 
 import Card from "../UI/Card/Card";
 import classes from "./Home.module.css";
-import Button from "../UI/Button/Button"
+import Button from "../UI/Button/Button";
 const Home = (props) => {
-  const authContext = useContext(AuthContext)
+	const authContext = useContext(AuthContext);
 
 	return (
 		<Card className={classes.home}>
 			<h1>Welcome back!</h1>
-      <Button onCLick={authContext.onLogout} >Logout</Button>
+			<Button onClick={authContext.onLogout}>Logout</Button>
 		</Card>
 	);
 };
